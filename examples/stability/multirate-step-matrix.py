73a9d92e1e86b4e123345d7ec425e51f606b6da3
Andreas Kloeckner <inform@tiker.net>
2015-11-23 00:56:41 -0600

    Add step matrix experiments

---
+++ examples/stability/multirate-step-matrix.py
@@ -0,0 +1,69 @@
+import numpy as np
+import numpy.linalg as la
+from leap.multistep.multirate import TwoRateAdamsBashforthMethod
+
+
+def main():
+    from leap.step_matrix import StepMatrixFinder
+
+    from pymbolic import var
+
+    speed_factor = 10
+    step_ratio = 6
+
+    print("speed factor: %g - step ratio: %g"
+            % (speed_factor, step_ratio))
+
+    method = TwoRateAdamsBashforthMethod(
+            method="Fq", order=3, step_ratio=step_ratio,
+            static_dt=True)
+
+    code = method.generate()
+
+    finder = StepMatrixFinder(code,
+            function_map={
+                "<func>f2f": lambda t, f, s: var("f2f") * f,
+                "<func>s2f": lambda t, f, s: var("s2f") * s,
+                "<func>f2s": lambda t, f, s: var("f2s") * f,
+                "<func>s2s": lambda t, f, s: var("s2s") * s,
+                },
+            exclude_variables=["<p>bootstrap_step"])
+
+    mat = finder.get_state_step_matrix("primary")
+
+    print('Variables: %s' % finder.variables)
+    if 0:
+        print('Variables: %s' % finder.variables)
+        np.set_printoptions(formatter={"all": str})
+        print(mat)
+
+    tol = 1e-8
+
+    def is_stable(direction, dt):
+        from pymbolic import evaluate
+        smat = np.asarray(
+                evaluate(mat, {
+                    "<dt>": dt,
+                    "f2f": direction,
+                    "s2f": 1/speed_factor,
+                    "f2s": 1/speed_factor,
+                    "s2s": direction*1/speed_factor,
+                    }),
+                dtype=np.complex128)
+
+        eigvals = la.eigvals(smat)
+
+        return (np.abs(eigvals) <= 1 + tol).all()
+
+    from leap.stability import find_truth_bdry
+    from functools import partial
+
+    prec = 1e-5
+    print("stable imaginary timestep:",
+            find_truth_bdry(partial(is_stable, 1j), prec=prec))
+    print("stable neg real timestep:",
+            find_truth_bdry(partial(is_stable, -1), prec=prec))
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/stability/step-matrix.py
@@ -0,0 +1,54 @@
+import numpy as np
+import numpy.linalg as la
+from leap.rk import RK4Method  # noqa
+from leap.multistep import AdamsBashforthMethod  # noqa
+
+
+def main():
+    from leap.step_matrix import StepMatrixFinder
+
+    from pymbolic import var
+
+    #method = RK4Method("y")
+    method = AdamsBashforthMethod("y", order=3, static_dt=True)
+
+    code = method.generate()
+
+    print(code)
+
+    def rhs_sym(t, y):
+        return var("lambda")*y
+
+    finder = StepMatrixFinder(code, function_map={"<func>y": rhs_sym},
+            exclude_variables=["<p>step"])
+
+    mat = finder.get_state_step_matrix("primary")
+
+    print('Variables: %s' % finder.variables)
+    np.set_printoptions(formatter={"all": str})
+    print(mat)
+
+    tol = 1e-8
+
+    def is_stable(direction, dt):
+        from pymbolic import evaluate
+        smat = np.asarray(
+                evaluate(mat, {"<dt>": dt, "lambda": direction}),
+                dtype=np.complex128)
+
+        eigvals = la.eigvals(smat)
+
+        return (np.abs(eigvals) <= 1 + tol).all()
+
+    from leap.stability import find_truth_bdry
+    from functools import partial
+
+    prec = 1e-5
+    print("stable imaginary timestep:",
+            find_truth_bdry(partial(is_stable, 1j), prec=prec))
+    print("stable neg real timestep:",
+            find_truth_bdry(partial(is_stable, -1), prec=prec))
+
+
+if __name__ == "__main__":
+    main()
