c3a40ee498e4283e29ba6736297350f01a39df5c
Matt Wala <wala1@illinois.edu>
2018-03-01 20:23:24 -0600

    [ci skip] Bump dagrt version in setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def main():
               "pytools>=2014.1",
               "pymbolic>=2014.1",
               "pytest>=2.3",
-              "dagrt",
+              "dagrt>=2018.1",
               "mako",
               "six",
               ],
