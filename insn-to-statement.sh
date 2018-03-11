f4bf57c71bcb207330bb87f6a6b73828a18b5b40
Andreas Kloeckner <inform@tiker.net>
2018-03-11 18:29:50 -0500

    Remove no-longer-needed insn-to-statement script

--- insn-to-statement.sh
+++
@@ -1,7 +0,0 @@
-set -x
-set -e
-sed -i s/Instruction/Statement/g $(git ls-files | grep -v insn-to-statement.sh | grep -v .txt )
-sed -i s/instruction/statement/g $(git ls-files | grep -v insn-to-statement.sh | grep -v .txt )
-sed -i s/INSTRUCTION/STATEMENT/g $(git ls-files | grep -v insn-to-statement.sh | grep -v .txt )
-sed -i s/insn/stmt/g $(git ls-files | grep -v insn-to-statement.sh | grep -v .txt )
-#patch -p1 < ./stmt-compat-fixes.patch
