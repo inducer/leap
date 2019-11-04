9491d324f2fb08e78543741d293f66b246ac1508
Matt Wala <wala1@illinois.edu>
2019-11-04 17:44:49 -0600

    Fix (maybe) wave-accuracy.py

--- examples/variable-coeff-wave-equation/wave-accuracy.py
+++ examples/variable-coeff-wave-equation/wave-accuracy.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-"""Runs a multirate accuracy experiment for the 1D wave equation."""
+"""Runs a multirate accuracy experiment for the 1D wave equation
+
+(This file is here to run the code as a CI job. See the file wave-equation.py
+for more experiments.)
+"""
 
 import os
 import subprocess
 
 
 if __name__ == "__main__":
-    subprocess.check_output(
-            ["./wave-equation.py", "-x", "accuracy"],
-            cwd=os.path.dirname(__file__))
+    subprocess.check_output(["./wave-equation.py", "-x", "accuracy"])
