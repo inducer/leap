68c97e3e26ab9d7241dd97f38f1d8b980dfc4b52
Andreas Klöckner <inform@tiker.net>
2020-02-06 07:54:47 +0100

    Add pylint bug link to test_step_matrix no-member pylint disable (Thanks @mattwala!)

--- test/test_step_matrix.py
+++ test/test_step_matrix.py
@@ -197,6 +197,7 @@ def test_step_matrix_sparse():
         sparse=True)
 
     assert mat.shape == (3, 3)
+    # https://github.com/PyCQA/pylint/issues/3388
     assert mat.indices == [(0, 0), (1, 1), (2, 2)]  # pylint: disable=no-member
     true_mat = np.eye(3, dtype=np.object) + dt * J
     assert (mat.data == np.diag(true_mat)).all()
