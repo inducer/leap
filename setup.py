d4cd4ae5a60514856afc962e7b824270922d0832
Matt Wala <wala1@illinois.edu>
2020-06-22 23:37:47 -0500

    Revert "Require Py3.6"
    This reverts commit e1aacebffaf9a68a45b2e5a9ff9c73202db0c1f2.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main():
+    from setuptools import setup, find_packages
+
+    version_dict = {}
+    init_filename = "leap/version.py"
+    exec(compile(open(init_filename, "r").read(), init_filename, "exec"),
+            version_dict)
+
+    setup(name="leap",
+          version=version_dict["VERSION_TEXT"],
+          description="Time integration by code generation",
+          long_description=open("README.rst", "rt").read(),
+          author="Andreas Kloeckner",
+          author_email="inform@tiker.net",
+          license="MIT",
+          url="http://wiki.tiker.net/Leap",
+          classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Other Audience',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Natural Language :: English',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3',
+              'Topic :: Scientific/Engineering',
+              'Topic :: Scientific/Engineering :: Information Analysis',
+              'Topic :: Scientific/Engineering :: Mathematics',
+              'Topic :: Scientific/Engineering :: Visualization',
+              'Topic :: Software Development :: Libraries',
+              'Topic :: Utilities',
+              ],
+
+          packages=find_packages(),
+
+          install_requires=[
+              "numpy>=1.5",
+              "pytools>=2014.1",
+              "pymbolic>=2014.1",
+              "pytest>=2.3",
+              "dagrt>=2019.4",
+              "mako",
+              "six",
+              ],
+          )
+
+
+if __name__ == '__main__':
+    main()
