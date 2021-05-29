84ccc497752a99181bd23432504cbb8d9214877e
Andreas Kloeckner <inform@tiker.net>
2021-05-29 17:45:11 -0500

    Black setup.py

--- setup.py
+++ setup.py
@@ -6,47 +6,45 @@ def main():
 
     version_dict = {}
     init_filename = "leap/version.py"
-    exec(compile(open(init_filename).read(), init_filename, "exec"),
-            version_dict)
+    exec(compile(open(init_filename).read(), init_filename, "exec"), version_dict)
 
-    setup(name="leap",
-          version=version_dict["VERSION_TEXT"],
-          description="Time integration by code generation",
-          long_description=open("README.rst").read(),
-          author="Andreas Kloeckner",
-          author_email="inform@tiker.net",
-          license="MIT",
-          url="http://wiki.tiker.net/Leap",
-          classifiers=[
-              'Development Status :: 3 - Alpha',
-              'Intended Audience :: Developers',
-              'Intended Audience :: Other Audience',
-              'Intended Audience :: Science/Research',
-              'License :: OSI Approved :: MIT License',
-              'Natural Language :: English',
-              'Programming Language :: Python',
-              'Programming Language :: Python :: 3',
-              'Topic :: Scientific/Engineering',
-              'Topic :: Scientific/Engineering :: Information Analysis',
-              'Topic :: Scientific/Engineering :: Mathematics',
-              'Topic :: Scientific/Engineering :: Visualization',
-              'Topic :: Software Development :: Libraries',
-              'Topic :: Utilities',
-              ],
-
-          packages=find_packages(),
-
-          python_requires="~=3.6",
-          install_requires=[
-              "numpy>=1.5",
-              "pytools>=2014.1",
-              "pymbolic>=2014.1",
-              "pytest>=2.3",
-              "dagrt>=2019.4",
-              "mako",
-              ],
-          )
+    setup(
+        name="leap",
+        version=version_dict["VERSION_TEXT"],
+        description="Time integration by code generation",
+        long_description=open("README.rst").read(),
+        author="Andreas Kloeckner",
+        author_email="inform@tiker.net",
+        license="MIT",
+        url="http://wiki.tiker.net/Leap",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Other Audience",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Topic :: Scientific/Engineering :: Visualization",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Utilities",
+        ],
+        packages=find_packages(),
+        python_requires="~=3.6",
+        install_requires=[
+            "numpy>=1.5",
+            "pytools>=2014.1",
+            "pymbolic>=2014.1",
+            "pytest>=2.3",
+            "dagrt>=2019.4",
+            "mako",
+        ],
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
